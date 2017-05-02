10105738b79f9a9f98853f547da96f5ab4ca0570
Leo Diaz <leoangelo.diaz@gmail.com>
2017-05-02 18:07:15 +0800

    debugging

--- src/main/java/com/huzzl/cli/Migrator.java
+++ src/main/java/com/huzzl/cli/Migrator.java
@@ -6,9 +6,10 @@ public class Migrator {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("afdsafas" + "\n\n\n asfasfda" + System.getenv("JDBC_DATABASE_USERNAME"));
+        System.out.println("\n\n\n database url: " + "jdbc:postgresql://" + System.getenv("JDBC_DATABASE_URL"));
+        System.out.println("ENVI: " + System.getenv("ENVI"));
 
-        if(System.getenv("envi") == "prod") {
+        if(System.getenv("ENVI") == "prod") {
             Flyway flyway = new Flyway();
             flyway.setDataSource("jdbc:postgresql://" + System.getenv("JDBC_DATABASE_URL"),
                     System.getenv("JDBC_DATABASE_USERNAME"),
