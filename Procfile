cce70c9fc0232f6e7220b737346f49b8dc3fb798
Leo Diaz <leoangelo.diaz@gmail.com>
2017-05-03 00:51:09 +0800

    debugging

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java $JAVA_OPTS -Ddw.server.applicationConnectors[0].port='$PORT' -jar build/libs/huzzl-back-1.0.1-SNAPSHOT-standalone.jar server ./src/main/config/production.yml
+web: build/install/huzzle-back/bin/huzzle-back
 migrate: java $JAVA_OPTS -jar build/libs/migrator.jar
--- src/main/java/com/huzzl/cli/Migrator.java
+++ src/main/java/com/huzzl/cli/Migrator.java
@@ -9,13 +9,15 @@ public class Migrator {
         System.out.println("\n\n\n database url: " + "jdbc:postgresql://" + System.getenv("JDBC_DATABASE_URL"));
         System.out.println("ENVI: " + System.getenv("ENVI"));
 
-        if(System.getenv("ENVI") == "prod") {
-            Flyway flyway = new Flyway();
-            flyway.setDataSource("jdbc:postgresql://" + System.getenv("JDBC_DATABASE_URL"),
-                    System.getenv("JDBC_DATABASE_USERNAME"),
-                    System.getenv("JDBC_DATABASE_PASSWORD"));
-            flyway.migrate();
-        }
+        String JDBC_DATABASE_URL        = ( System.getenv("JDBC_DATABASE_URL") == null ? "jdbc:postgresql://192.168.1.50/huzzldb" : System.getenv("JDBC_DATABASE_URL"));
+        String JDBC_DATABASE_USERNAME   = ( System.getenv("JDBC_DATABASE_USERNAME") == null ? "devuser" : System.getenv("JDBC_DATABASE_USERNAME"));
+        String JDBC_DATABASE_PASSWORD   = ( System.getenv("JDBC_DATABASE_PASSWORD") == null ? "demo123" : System.getenv("JDBC_DATABASE_PASSWORD"));
+
+        Flyway flyway = new Flyway();
+
+        flyway.setDataSource( JDBC_DATABASE_URL, JDBC_DATABASE_USERNAME, JDBC_DATABASE_PASSWORD);
+
+        flyway.migrate();
 
     }
 }
