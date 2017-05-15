0ac8b3bd761aa3d8fafcd5b35bf454f75f39c065
Leo Diaz <leoangelo.diaz@gmail.com>
2017-05-16 02:13:00 +0800

    fixing header issue

--- src/main/java/com/huzzl/MainApplication.java
+++ src/main/java/com/huzzl/MainApplication.java
@@ -137,7 +137,7 @@ public class MainApplication extends Application<MainConfiguration> {
                 env.servlets().addFilter("CORS", CrossOriginFilter.class);
 
         cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With, Content-Type, Accept, Origin, Authorization, Cache-Control");
         cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
 
         // Add URL Mapping
